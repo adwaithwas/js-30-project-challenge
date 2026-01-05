3363bc8fec41a3e32567ae022dd4eb45da175d40
adwaithwas <betteradwaith@gmail.com>
2026-01-05 11:04:12 +0530

    added project 4

---
+++ day-04-bg-color-changer/script.js
@@ -0,0 +1,11 @@
+function changeColor(color){
+    document.body.style.backgroundColor = color;
+}
+
+function randomColor(){
+    let red = Math.round(Math.random()*255);
+    let green = Math.round(Math.random()*255);
+    let blue = Math.round(Math.random()*255);
+
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+}
